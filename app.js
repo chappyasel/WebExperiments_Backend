b697b1ee601e83f2a90161281f2bd5f08c55c322
aselg <aselg@washington.edu>
2019-03-26 23:29:11 -0400

    Started work on endpoint structure

--- app.js
+++ app.js
@@ -1,44 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const server = require('http').Server(app);
-const port = 3000;
-const helmet = require('helmet');
-var path = require('path');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const path = './weightliftingapp/v1/'
+const users = require(path + 'users')
 
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
-	next();
-});
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+	next()
+})
 
-//pre-flight requests
-app.options('*', function(req, res) {
-	res.send(200);
-});
+server.listen(3000, (err) => {
+	if (err) throw err
+	console.log('Web Experiments activated!')
+})
 
-app.use('/', express.static(__dirname + '/public'));
+// before every endpoint
+app.options('*', (req, res) => {
+	res.send(200)
+})
 
-server.listen(port, (err) => {
-	if (err) {
-		throw err;
-	}
-	/* eslint-disable no-console */
-	console.log('Node Endpoints working :)');
-});
+app.use('/', express.static(__dirname + '/public'))
 
-var x = 0;
-
-app.get('/nodetest/', (err, res) => {
-    x ++;
-	res.status(200);
-	res.json({ viewers: x });
-	res.end();
-});
-
-module.exports = server;
+app.use('/users', users)
