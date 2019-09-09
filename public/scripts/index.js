1a0a4c83fe11826958ad91abfe13ae4a51934feb
Chappy Asel <chappy_asel@hotmail.com>
2019-09-08 20:48:03 -0700

    Improved index page, added Vue js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,30 +1,38 @@
+import about from '../src/about.js'
+import projects from '../src/projects.js'
 
-(function() {
-	
-	const MOBILE_SIZE = 750;
-	
-	"use strict";
-	
-	$(document).ready(function() {
-		$("aside").click(menuPressed);
-		$(".menu-btn").click(function() { menuButtonPressed(this) });
-	});
-	
-	function menuPressed() {
-		if(isMobile()) {
-			$("aside").toggleClass("full");
+const MOBILE_SIZE = 750
+
+new Vue({
+	el: '#app-menu',
+	methods: {
+		dropMenu: function() {
+			if (isMobile()) {
+				$("aside").toggleClass("full")
+			}
+		},
+		menuClicked: function(segment) {
+			let offset = 20 + (isMobile() ? 40 : 0)
+			$("html, body").animate({
+				scrollTop: $("article." + segment).offset().top - offset
+			}, 750)
 		}
 	}
-	
-	function menuButtonPressed(button) {
-		let offset = 20 + (isMobile() ? 40 : 0);
-		$("html, body").animate({
-			scrollTop: $("article." + button.id).offset().top - offset
-		}, 750);
+})
+
+
+new Vue({
+	el: '#app-index',
+	data: {
+		about: about,
+		projects: projects
+	},
+	methods: {
+		
 	}
-	
-	function isMobile() {
-		return $(window).width() <= MOBILE_SIZE;
-	}
-	
-})();
+})
+
+
+function isMobile() {
+	return $(window).width() <= MOBILE_SIZE
+}
