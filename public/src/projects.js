f9940a4ee11910de0155e1cb6b5e15ecfbeea81d
Chappy Asel <chappy_asel@hotmail.com>
2019-09-25 20:41:36 -0700

    New projects style, misc fixes

--- public/src/projects.js
+++ public/src/projects.js
@@ -11,37 +11,37 @@ export default {
       {
          name: 'Weightlifting App',
          link: 'https://apps.apple.com/us/app/weightlifting-app/id1266077653',
-         image: 'placeholder.jpg',
+         image: 'weightlifting.jpg',
          languages: ['Objective C']
       },
       {
          name: 'Homework App',
          link: 'https://apps.apple.com/us/app/homework-app/id1097438761',
-         image: 'placeholder.jpg',
+         image: 'homework.jpg',
          languages: ['Objective C']
       },
       {
          name: 'Web Experiments',
          link: 'https://github.com/ChappyA12/WebExperiments_Backend',
-         image: 'placeholder.jpg',
+         image: 'web-exp.jpg',
          languages: ['HTML', 'CSS', 'JavaScript', 'Node.js']
       },
       {
          name: 'Fantasy Basketball',
          link: 'https://github.com/ChappyA12/FantasyBasketball_iOS',
-         image: 'placeholder.jpg',
+         image: 'fantasy.jpg',
          languages: ['Objective C']
       },
       {
          name: 'Weightlifting App ML',
          link: 'https://github.com/ChappyA12/WeightliftingAppML_Python',
-         image: 'placeholder.jpg',
+         image: 'weightlifting-ml.jpg',
          languages: ['Python']
       },
       {
          name: 'Rotoworld ML',
          link: 'https://github.com/ChappyA12/RotoworldML_Python',
-         image: 'placeholder.jpg',
+         image: 'rotoworld-ml.jpg',
          languages: ['Python']
       }
    ]
