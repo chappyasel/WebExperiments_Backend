6be1c9bd90c15fa0db78990ec92e8063116e9f1a
Chappy Asel <chappy_asel@hotmail.com>
2019-09-27 17:42:59 -0700

    Added about copy, small style changes

--- public/src/about.js
+++ public/src/about.js
@@ -1,12 +1,34 @@
 export default {
     description: `
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-    when an unknown printer took a galley of type and scrambled it to make a type 
-    specimen book. It has survived not only five centuries, but also the leap into 
-    electronic typesetting, remaining essentially unchanged. It was popularised in 
-    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-    and more recently with desktop publishing software like Aldus PageMaker 
-    including versions of Lorem Ipsum.
+    Hi, I'm Chappy 👋
+    <br/><br/>
+    I'm a self-driven full stack developer eager to try new things and continue
+    to expand on my areas of knowledge. I am currently studying Computer Science
+    at the University of Washington and pursuing a position as an intern at an
+    exciting, driven company for this upcoming summer.
+    <br/><br/>
+    Since the beginning of high school I've had the priviledge of working for a
+    variety of companies, including
+    [Xevo](https://www.xevo.com), 
+    [Suplari](https://www.suplari.com), 
+    [Lime](https://www.li.me)
+    among many others. While working at each of these companies, I have gained
+    insights into how startups scale and function and how effective leaders are
+    forged from positive, electric work environments.  
+    <br/><br/>
+    Beyond programming and improving people's lives by solving real-world problems,
+    I enjoy challenging myself physically via weightlifting, organized
+    endurance events, and hiking. I also enjoy spending time with my family and 
+    friends whenever I find the time.
+    <br/><br/>
+    Below is a list of some of my recent personal projects. For more details on
+    each project, feel free to click on each GitHub link. If you have any further
+    questions regarding my past work experience or credentials please feel free
+    to check out my 
+    [LinkedIn](https://www.linkedin.com/in/chappyasel/)
+    or
+    [reach out](mailto:chappy_asel@hotmail.com) 
+    via the email listed at the top of my resume.
     `
+    .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" target="_blank">$1</a>')
 }
