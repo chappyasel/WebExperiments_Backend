ad66cb77b824cb7a95522ddd12a424c2549b9af5
Chappy Asel <chappy_asel@hotmail.com>
2022-05-01 04:30:19 -0700

    Full support for inputs

--- client/src/liarsdice/LiarsDice.js
+++ client/src/liarsdice/LiarsDice.js
@@ -1,58 +1,124 @@
 import React, { useState, useEffect } from 'react'
 import { createGlobalStyle } from 'styled-components'
+import { Table, Form, Stack } from 'react-bootstrap'
 const fetch = require('node-fetch')
 
 const GlobalStyle = createGlobalStyle`
   html {
     font-family: sans-serif;
+    margin: 20px;
   }
 `
 
 export default function LiarsDice() {
+  const [input, setInput] = useState({
+    myDice: [2, 0, 1, 0, 0, 2],
+    totalDice: 20,
+    countOnes: true,
+  })
   const [targets, setTargets] = useState([])
 
   useEffect(() => {
-    const loadGame = async () => {
-      const input = {
-        myDice: [2, 0, 1, 0, 0, 1],
-        totalDice: 20,
-        countOnes: true,
+    const playGame = async () => {
+      const output = await play(input)
+      if (output?.targets !== undefined) {
+        setTargets(output.targets)
       }
-      const output = await play(input.myDice, input.totalDice, input.countOnes)
-      setTargets(output.targets)
     }
 
-    loadGame()
-  }, [])
+    playGame()
+  }, [input])
 
   return (
     <>
       <GlobalStyle />
+      <Form className="mb-5">
+        <Stack direction="horizontal">
+          {input.myDice.map((numDice, index) => (
+            <Form.Group key={index} className="mb-3" style={{ width: '100px' }}>
+              <Form.Label>{numberToString(index)}</Form.Label>
+              <Form.Control
+                type="number"
+                value={numDice}
+                onChange={e => {
+                  const myDice = input.myDice
+                  myDice[index] = parseInt(e.target.value)
+                  setInput({ ...input, myDice })
+                }}
+              />
+            </Form.Group>
+          ))}
+        </Stack>
+        <Form.Group className="mb-3">
+          <Form.Label>Dice left</Form.Label>
+          <Form.Control
+            type="number"
+            value={input.totalDice}
+            onChange={e => setInput({ ...input, totalDice: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Check
+          type="checkbox"
+          label="count ones"
+          checked={input.countOnes}
+          onChange={e => setInput({ ...input, countOnes: e.target.checked })}
+        />
+      </Form>
       {targets.map(target => (
         <div>
-          <p>
-            Odds for {target.diceNumber} (you already have {target.alreadyHave}):
-          </p>
-          {target.scenarios.map(scenario => {
-            const probability = (scenario.probability * 100).toFixed(2)
-            const spotOn = (scenario.spotOnProbability * 100).toFixed(2)
-            return (
-              <p>
-                &nbsp;{scenario.numMatches}:&nbsp;{probability}% (spot on: {spotOn}%)
-              </p>
-            )
-          })}
+          <h1>
+            Odds for {target.diceNumber} (you have {target.alreadyHave}):
+          </h1>
+          <Table key={target.diceNumber} bordered striped size="sm">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Probability</th>
+                <th>Spot On</th>
+              </tr>
+            </thead>
+            <tbody>
+              {target.scenarios.map(scenario => {
+                const probability = (scenario.probability * 100).toFixed(2)
+                const spotOn = (scenario.spotOnProbability * 100).toFixed(2)
+                return (
+                  <tr>
+                    <td>{scenario.numMatches}</td>
+                    <td>{probability}%</td>
+                    <td>{spotOn}%</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
         </div>
       ))}
     </>
   )
 }
 
-const play = async (myDice, totalDice, countOnes) =>
+const play = async input =>
   await fetch('api/liarsdice/v1/play', {
     method: 'POST',
-    body: JSON.stringify({ myDice, totalDice, countOnes }),
+    body: JSON.stringify(input),
     headers: { 'Content-Type': 'application/json' },
   })
     .then(res => res.json())
-    .catch(err => alert('error', err))
+    .catch(err => alert(`error: ${err}`))
+
+const numberToString = number => {
+  switch (number) {
+    case 0:
+      return 'Ones'
+    case 1:
+      return 'Twos'
+    case 2:
+      return 'Threes'
+    case 3:
+      return 'Fours'
+    case 4:
+      return 'Fives'
+    case 5:
+      return 'Sixes'
+  }
+}
